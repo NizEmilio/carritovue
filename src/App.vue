5efbb567476eb7fb3559412cf48dcec0de1ae19e
NizEmilio <61974505+NizEmilio@users.noreply.github.com>
2021-03-03 12:25:25 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <AppProductList />
+    <AppShoppingCart />
+    <AppProductEdit />
   </div>
 </template>
 
+<script>
+import AppProductList from "./components/AppProductList.vue";
+import AppShoppingCart from "./components/AppShoppingCart.vue";
+import AppProductEdit from "./components/AppProductEdit.vue";
+export default {
+  name: "app",
+  components: {
+    AppProductList,
+    AppShoppingCart,
+    AppProductEdit
+  }
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
-
-#nav {
-  padding: 30px;
+#app > * {
+  flex-basis: 49%;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app div:last-of-type {
+  flex-basis: 100%;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,38 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import shop from "../api/shop.js";
+import cart from "./cart.js";
+import products from "./products.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
+  modules: {
+    cart,
+    products
+  },
   state: {
+    checkoutError: false
   },
   mutations: {
+    setCheckoutError(state, error) {
+      state.checkoutError = error;
+    }
   },
   actions: {
+    checkout({ commit, state }) {
+      shop.buyProducts(
+        state.cart.cart,
+        () => {
+          commit("emptyCart");
+          commit("setCheckoutError", false);
+        },
+        () => {
+          commit("setCheckoutError", true);
+        }
+      );
+    }
   },
-  modules: {
-  }
-})
+  getters: {}
+});
